e402637245d870bbdcac17da2c6954a795dc1107
Victor Smeets <victor.smeets@student.kuleuven.be>
2021-05-16 15:10:31 +0200

    push it

--- Matlab/muffler_design.m
+++ Matlab/muffler_design.m
@@ -1,14 +1,14 @@
 function muffler_design(cte)
 
 % coordinates:
-inlet = [0 137];
-ExpCh = inlet(2); % change if extended inlet
-outlet = [ExpCh+cte.L*10^3 400];
+ExpCh = [-106.5 106.5];
+inlet = [ExpCh(1)-50 ExpCh(1)];
+outlet = [ExpCh(2) ExpCh(1)+50];
 
-mic_A = ExpCh-(cte.m1+cte.s1)*10^3;
-mic_B = ExpCh-cte.m1*10^3;
-mic_C = ExpCh+(cte.L+cte.m2)*10^3;
-mic_D = ExpCh+(cte.L+cte.m2+cte.s2)*10^3;
+mic_A = ExpCh(1)-(cte.m1+cte.s1)*10^3;
+mic_B = ExpCh(1)-cte.m1*10^3;
+mic_C = ExpCh(2)+(cte.L+cte.m2)*10^3;
+mic_D = ExpCh(2)+(cte.L+cte.m2+cte.s2)*10^3;
 HR_1 = 36;
 HR_2 = 88;
 
@@ -18,20 +18,20 @@ plot(inlet,1000*[-cte.D/2 -cte.D/2],'b-'),
 plot(inlet,1000*[cte.D/2 cte.D/2],'b-'),
 plot([inlet(1) inlet(1)],1000*[-cte.D/2 cte.D/2],'b-'),
 
-plot([ExpCh ExpCh],1000*[cte.D/2 cte.D*5/2],'b-'),
-plot([ExpCh ExpCh],1000*[-cte.D/2 -cte.D*5/2],'b-'),
-plot([ExpCh ExpCh+cte.L*10^3],1000*[-cte.D*5/2 -cte.D*5/2],'b-'),
-plot([ExpCh ExpCh+cte.L*10^3],1000*[cte.D*5/2 cte.D*5/2],'b-'),
-plot([ExpCh+cte.L*10^3 ExpCh+cte.L*10^3],1000*[cte.D/2 cte.D*5/2],'b-'),
-plot([ExpCh+cte.L*10^3 ExpCh+cte.L*10^3],1000*[-cte.D/2 -cte.D*5/2],'b-'),
-
-plot(outlet,1000*[-cte.D/2 -cte.D/2],'b-'),
-plot(outlet,1000*[cte.D/2 cte.D/2],'b-'),
-plot([outlet(2) outlet(2)],1000*[-cte.D/2 cte.D/2],'b-'),
+plot([ExpCh(1) ExpCh(1)],1000*[cte.D/2 cte.D*5/2],'b-'),
+plot([ExpCh(1) ExpCh(1)],1000*[-cte.D/2 -cte.D*5/2],'b-'),
+plot(ExpCh,1000*[-cte.D*5/2 -cte.D*5/2],'b-'),
+plot(ExpCh,1000*[cte.D*5/2 cte.D*5/2],'b-'),
+plot([ExpCh(2) ExpCh(2)],1000*[cte.D/2 cte.D*5/2],'b-'),
+plot([ExpCh(2) ExpCh(2)],1000*[-cte.D/2 -cte.D*5/2],'b-'),
+% 
+% plot(outlet,1000*[-cte.D/2 -cte.D/2],'b-'),
+% plot(outlet,1000*[cte.D/2 cte.D/2],'b-'),
+% plot([outlet(2) outlet(2)],1000*[-cte.D/2 cte.D/2],'b-'),
 
 plot([mic_A mic_B mic_C mic_D],[0 0 0 0],'ro'),
 
-axis([-5 405 -205 205])
+axis([-200 200 -205 205])
 
 end
 
